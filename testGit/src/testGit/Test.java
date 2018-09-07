9e4de1928ace5548c9dfecbba3ae3f1917e4f001
kwmo <kwmo@kevin_lenovo>
2018-09-07 11:23:48 +0800

    第一次提交

---
+++ testGit/src/testGit/Test.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:testGit
+ * File Name:Test.java
+ * Package Name:testGit
+ * Date:2018年9月7日上午11:18:23
+ * Copyright (c) 2018, chenzhou1025@126.com All Rights Reserved.
+ *
+*/
+
+package testGit;
+/**
+ * ClassName:Test <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年9月7日 上午11:18:23 <br/>
+ * @author   kwmo
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class Test {
+  public static void main(String[] args) {
+	System.out.println("第一次测试git");
+}
+}
+
