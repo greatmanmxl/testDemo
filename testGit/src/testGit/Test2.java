8106550a3a6d9e94322c8c37bf97e279cdc5ff92
kwmo <kwmo@kevin_lenovo>
2018-09-07 11:27:27 +0800

    第二个测试文件

---
+++ testGit/src/testGit/Test2.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:testGit
+ * File Name:Test2.java
+ * Package Name:testGit
+ * Date:2018年9月7日上午11:27:06
+ * Copyright (c) 2018, chenzhou1025@126.com All Rights Reserved.
+ *
+*/
+
+package testGit;
+/**
+ * ClassName:Test2 <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2018年9月7日 上午11:27:06 <br/>
+ * @author   kwmo
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class Test2 {
+
+}
+
